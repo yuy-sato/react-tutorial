fe8513d865a0fcdcd55f5da049b47b2de305f60b
yuya.sato <yuya.sato@livesense.co.jp>
2022-06-13 19:35:36 +0900

    1ファイル1コンポーネントに分割

---
+++ src/components/MoveList.js
@@ -0,0 +1,19 @@
+const MoveList = (props) => {
+  return (
+    <ol>
+      {
+        props.history.map((_, move) => {
+          const desc = move ? `Go to move #${move}` : 'Go to game start';
+
+          return (
+            <li key={move}>
+              <button onClick={() => props.onClick(move)}>{desc}</button>
+            </li>
+          );
+        })
+      }
+    </ol>
+  );
+}
+
+export default MoveList;
